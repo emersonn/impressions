1aef0d6adda7cdda2232487772ddd10b499c55ad
Emerson Matson <emersonn@cs.washington.edu>
2016-02-28 13:27:42 -0800

    added game playing

--- impressions/views.py
+++ impressions/views.py
@@ -58,7 +58,8 @@ def index():
         current_game = Game(start=datetime.datetime.now())
 
         for key in request.form.keys():
-            if key is not 'category':
+            if key != 'category':
+                print(key)
                 current_player = User(name=request.form[key], points=0)
                 current_game.players.append(current_player)
 
@@ -71,7 +72,7 @@ def index():
         response = json.loads(r.text)
         results = response['d']['results']  # MediaUrl
 
-        for result in results[:5]:
+        for result in results[:min(5, len(results))]:
             current_round = Round(
                 impression=result['MediaUrl']
             )
@@ -92,7 +93,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/game/<game_id>')
+@app.route('/game/<game_id>', methods=['GET', 'POST'])
 def game(game_id):
     """Game playing page of the website given the game_id.
 
@@ -106,41 +107,38 @@ def game(game_id):
     """
 
     current_game = get_game_or_abort(game_id)
-    print(current_game.rounds)
-    print(current_game.current_round)
+    # print(current_game.rounds)
+    # print(current_game.current_round)
     if current_game.current_round is None:
         return redirect(url_for('results', game_id=game_id), 303)
 
     if request.method == 'POST':
         # TODO(There should be a lot more cases for safety, and security.)
-        if 'round_id' not in request.form or 'winner_id' not in request.form:
+        if 'playervote' not in request.form:
             abort(406)
 
-        current_round = (
-            db.session
-            .query(Round).get(request.form['round_id'])
-        )
+        current_round = db.session.query(Round).get(current_game.current_round)
 
         if not current_round:
             abort(404)
 
-        winner_id = request.form['winner_id']
+        winner_id = request.form['playervote']
         winner = db.session.query(User).get(winner_id)
 
         if not winner:
             abort(404)
 
-        current_round.winner = winner
+        current_round.winner = winner_id
         winner.points += 1
 
         if (
-            current_game.index(current_round) + 1 >=
+            current_game.rounds.index(current_round) + 1 >=
             len(current_game.rounds)
         ):
             current_game.current_round = None
-            return redirect(url_for('results', game_id), 303)
+            return redirect(url_for('results', game_id=game_id), 303)
         current_game.current_round = (
-            current_game.rounds[current_game.index(current_round) + 1]
+            current_game.rounds[current_game.rounds.index(current_round) + 1].id
         )
 
         db.session.add(current_game)
@@ -148,7 +146,8 @@ def game(game_id):
         db.session.add(current_round)
         db.session.commit()
 
-    current_round = current_game.current_round
+    current_round = db.session.query(Round).get(current_game.current_round)
+    print(current_round.impression)
     return render_template(
         'game.html',
         round=current_round,
