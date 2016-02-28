01bf730ab455d4798f5f8fe199b322e612e6591b
Emerson Matson <emersonn@cs.washington.edu>
2016-02-27 20:17:59 -0800

    added a lot of the game

--- impressions/views.py
+++ impressions/views.py
@@ -1,6 +1,7 @@
 from flask import abort
 from flask import redirect
 from flask import render_template
+from flask import request
 from flask import url_for
 
 from chemy import app
@@ -28,9 +29,30 @@ def game(game_id):
         abort(404)
     if not current_game.current_round:
         redirect(url_for('results', game_id), 303)
+    if request.method == 'POST':
+        # TODO(There should be a lot more cases for safety, and security.)
+        if 'round_id' in request.form and 'winner_id' in request.form:
+            current_round = (
+                db.session
+                .query(Round).get(request.form['round_id'])
+            )
+            if not current_round:
+                abort(404)
+            # TODO(Should check whether the winner ID exists in both ways)
+            winner_id = request.form['winner_id']
+            current_round.winner = current_game.players[winner_id]
+
+            # current_round needs .indexFor()
+            if current_game.current_round + 1 >= len(current_game.rounds):
+                redirect(url_for('results', game_id), 303)
+            current_game.current_round = pass
+        else:
+            abort(406)
+
+    current_round = current_game.current_round
     return render_template('static/game.html')
 
-
+"""
 @app.route('/game/<game_id>/<round_id>')
 def round(game_id, round_id):
     current_game = db.session.query(Game).get(game_id)
@@ -38,10 +60,15 @@ def round(game_id, round_id):
         abort(404)
     if not current_game.current_round:
         redirect(url_for('results', game_id), 303)
-    # if they don't have the right round
+
+    current_round = db.session.query(Round).get(round_id)
+    if not current_round:
+        abort(404)
+    if current_game.current_round != current_round:
     # if this is a post request to submit the winner
     # otherwise return the request
     pass
+"""
 
 
 @app.route('/results/<game_id>')
