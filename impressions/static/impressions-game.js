5c71cafb96a2a2c7c31d4ae1fe8a0fb0e112ae46
jackarms <jack.armstrong121@gmail.com>
2016-02-28 10:59:43 -0800

    removed fixed navbar

--- impressions/static/impressions-game.js
+++ impressions/static/impressions-game.js
@@ -1,16 +1,17 @@
 window.onload = function() {
-	var body = document.querySelector("body");
-	var padding = parseInt(window.getComputedStyle(document.querySelector("nav")).height);
-	body.style.paddingTop = padding + 5 + "px";
+	// var body = document.querySelector("body");
+	// var padding = parseInt(window.getComputedStyle(document.querySelector("nav")).height);
+	// body.style.paddingTop = padding + 5 + "px";
 
 	var radioButtons = document.getElementById("judging").querySelectorAll("input");
 	for(var i = 0; i < radioButtons.length; i++) {
 		radioButtons[i].onclick = updateVotedPlayer;
 	}
+
+	document.getElementById("vote").onclick = submitVote;
 };
 
 function updateVotedPlayer() {
-	console.log("updating selected player:");
 	var radioButtons = document.getElementById("judging").querySelectorAll("input");
 	for(var i = 0; i < radioButtons.length; i++) {
 		var parentDiv = radioButtons[i].parentNode;
@@ -20,4 +21,8 @@ function updateVotedPlayer() {
 	if(! parentDiv.classList.contains("votedplayer")) {
 		parentDiv.classList.add("votedplayer");
 	}
+}
+
+function submitVote() {
+	document.querySelector("form").submit();
 }
