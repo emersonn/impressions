1549c578dc9df0b0b4f9c5ca3f33f6e50e9927d5
Emerson Matson <emersonn@cs.washington.edu>
2016-02-28 13:28:11 -0800

    fixed game js

--- impressions/static/impressions-game.js
+++ impressions/static/impressions-game.js
@@ -9,7 +9,14 @@ window.onload = function() {
 	}
 
 	document.getElementById("vote").onclick = submitVote;
-	document.querySelector("li button").onclick = submitCaption;
+
+	var buttons = document.querySelectorAll("li button");
+	for(var i = 0; i < buttons.length; i++) {
+		buttons[i].onclick = submitCaption;
+	}
+
+	document.getElementById("judgebutton").onclick = judge;
+
 };
 
 function updateVotedPlayer() {
@@ -25,10 +32,31 @@ function updateVotedPlayer() {
 }
 
 function submitCaption() {
+	// console.log("butthole");
 	// var playerName = this.parentNode.querySelector(".listname").innerHTML;
 	this.parentNode.parentNode.querySelector(".captioninput").style.display = "none";
 	this.parentNode.parentNode.querySelector(".submittedtext").style.display = "";
 	this.parentNode.parentNode.style.backgroundColor = "#bbbbbb";
+}
+
+function judge() {
+	document.getElementById("submissions").style.display = "none";
+	document.getElementById("judging").style.display = "";
+
+	var captions = [];
+	var textareas = document.querySelectorAll("li textarea");
+	for(var i = 0; i < textareas.length; i++) {
+		console.log("pushing: " + textareas[i].value);
+		captions.push(textareas[i].value);
+	}
+
+	console.log(captions);
+	var quotes = document.querySelectorAll("#judging q");
+	for(var i = 0; i < quotes.length; i++) {
+		quotes[i].innerHTML = captions[i];
+	}
+
+	captions = null;
 }
 
 function submitVote() {
