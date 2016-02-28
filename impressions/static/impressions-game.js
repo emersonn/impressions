bdfe7313d47bb0ca2bb8dfe2bbbbe487a188c692
jackarms <jack.armstrong121@gmail.com>
2016-02-28 08:52:40 -0800

    web files

--- impressions/static/impressions-game.js
+++ impressions/static/impressions-game.js
@@ -2,5 +2,22 @@ window.onload = function() {
 	var body = document.querySelector("body");
 	var padding = parseInt(window.getComputedStyle(document.querySelector("nav")).height);
 	body.style.paddingTop = padding + 5 + "px";
+
+	var radioButtons = document.getElementById("judging").querySelectorAll("input");
+	for(var i = 0; i < radioButtons.length; i++) {
+		radioButtons[i].onclick = updateVotedPlayer;
+	}
 };
 
+function updateVotedPlayer() {
+	console.log("updating selected player:");
+	var radioButtons = document.getElementById("judging").querySelectorAll("input");
+	for(var i = 0; i < radioButtons.length; i++) {
+		var parentDiv = radioButtons[i].parentNode;
+		parentDiv.classList.remove("votedplayer");
+	}
+	var parentDiv = this.parentNode;
+	if(! parentDiv.classList.contains("votedplayer")) {
+		parentDiv.classList.add("votedplayer");
+	}
+}
--- impressions/static/impressions-setup.js
+++ impressions/static/impressions-setup.js
@@ -5,6 +5,7 @@ window.onload = function() {
 	var padding = parseInt(window.getComputedStyle(document.querySelector("nav")).height);
 	body.style.paddingTop = padding + 5 + "px";
 	document.getElementById("addplayer").onclick = addNewPlayer;
+	document.getElementById("start").onclick = submitForm;
 };
 
 function addNewPlayer() {
@@ -21,9 +22,13 @@ function addNewPlayer() {
 	// var incScoreButton = document.createElement("button");
 	// var decScoreButton = document.createElement("button");
 	removeButton.id = "removeplayer" + numPlayers;
-	removeButton.innerHTML = "x";
+	removeButton.innerHTML = "-";
 	removeButton.onclick = removePlayer;
-	removeButton.className = "remove";
+	//removeButton.className = "remove";
+	removeButton.classList.add("btn");
+	removeButton.classList.add("btn-lg");
+	removeButton.classList.add("btn-primary");
+	removeButton.type = "button";
 
 	playerNameDiv.className = "playername";
 	playerNameDiv.innerHTML = playerName;
@@ -35,24 +40,37 @@ function addNewPlayer() {
 	incScoreButton.innerHTML = "+";
 	decScoreButton.innerHTML = "-";*/
 
+	// playerNameDiv.style.marginTop = parseInt(window.getComputedStyle(document.getElementById("addplayerli")).height) / 2 + "px";
 	newPlayerDiv.appendChild(removeButton);
 	newPlayerDiv.appendChild(playerNameDiv);
 	// newPlayerDiv.appendChild(incScoreButton);
 	// newPlayerDiv.appendChild(decScoreButton);
-
+	newPlayerLi.id = "li-player-" + playerName;
 	newPlayerLi.appendChild(newPlayerDiv);
 
 	var playerList = document.getElementById("playerlist");
 	playerlist.insertBefore(newPlayerLi, document.getElementById("addplayerli"));
 
 	document.getElementById("addplayername").value = "";
+	
+	var nameInput = document.createElement("input");
+	nameInput.id = "input-player-" + playerName;
+	nameInput.type = "hidden";
+	nameInput.name = "names[player-" + playerName + "]";
+	document.querySelector("form").appendChild(nameInput);
 	numPlayers++;
 }
 
 function removePlayer() {
-	var toRemove = "player" + this.id[this.id.length - 1];
-	var toRemoveLi = document.getElementById(toRemove);
+	var toRemoveLi = this.parentNode.parentNode;
+	var base = "li-player-";
+	var playerName = toRemoveLi.id.substring(base.length);
 	toRemoveLi.parentNode.removeChild(toRemoveLi);
-
+	var inputToRemove = document.getElementById("input-player-" + playerName);
+	inputToRemove.parentNode.removeChild(inputToRemove);
 	numPlayers--;
+}
+
+function submitForm() {
+	document.querySelector("form").submit();
 }
